8aa1bf195fbc660201d3b82403670e22ddd2c270
ZackPedretti <zackpedretti52@gmail.com>
2026-06-11 15:17:49 +0200

    feat: add path variants to functions to use the s3Path instead of bucket and key

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,23 @@ export async function putObjectToS3(bucket: string, key: string, file: Buffer<Ar
   )
 }
 
+export function getBucketAndKeyFromS3Path(s3Path: string): { bucket: string, key: string } {
+  const [bucket, ...keyParts] = s3Path.split('/');
+  const key = keyParts.join('/');
+  if (bucket === undefined) {
+    throw new Error(`Could not determine bucket name from the s3Path: ${s3Path}`);
+  }
+  if (key.length === 0) {
+    throw new Error(`Could not determine key from the s3Path: ${s3Path}`);
+  }
+  return { bucket, key };
+}
+
+export async function putObjectToS3Path(s3Path: string, file: Buffer<ArrayBuffer>, s3Client: S3Client) {
+  const { bucket, key } = getBucketAndKeyFromS3Path(s3Path);
+  return putObjectToS3(bucket, key, file, s3Client);
+}
+
 export async function getObjectFromS3(bucket: string, key: string, s3Client: S3Client) {
   const response = await s3Client.send(
     new GetObjectCommand({
@@ -37,7 +54,12 @@ export async function getObjectFromS3(bucket: string, key: string, s3Client: S3C
   return response;
 }
 
-export async function* listObjectsGenerator(bucket: string, prefix: string, s3Client: S3Client, options?: { maxKeys?: number, delimiter?: string }) {
+export async function getObjectFromS3Path(s3Path: string, s3Client: S3Client) {
+  const { bucket, key } = getBucketAndKeyFromS3Path(s3Path);
+  return getObjectFromS3(bucket, key, s3Client);
+}
+
+async function* listObjectsGenerator(bucket: string, prefix: string, s3Client: S3Client, options?: { maxKeys?: number, delimiter?: string }) {
   let continuationToken: string | undefined;
 
   for (; ;) {
@@ -61,11 +83,16 @@ export async function* listObjectsGenerator(bucket: string, prefix: string, s3Cl
   }
 }
 
-export function getListObjectsFromS3(bucket: string, prefix: string, s3Client: S3Client, options?: { maxKeys?: number, delimiter?: string, }) {
+export function getListObjectsFromS3(bucket: string, prefix: string, s3Client: S3Client, options?: { maxKeys?: number, delimiter?: string }) {
   return Readable.from(listObjectsGenerator(bucket, prefix, s3Client, options), { objectMode: true });
 }
 
-export async function s3FileExists(bucket: string, key: string, s3Client: S3Client) {
+export function getListObjectsFromS3Path(s3Path: string, s3Client: S3Client, options?: { maxKeys?: number, delimiter?: string }) {
+  const { bucket, key } = getBucketAndKeyFromS3Path(s3Path);
+  return getListObjectsFromS3(bucket, key, s3Client, options);
+}
+
+export async function s3ObjectExists(bucket: string, key: string, s3Client: S3Client) {
   try {
     await s3Client.send(
       new HeadObjectCommand({ Bucket: bucket, Key: key })
@@ -76,10 +103,20 @@ export async function s3FileExists(bucket: string, key: string, s3Client: S3Clie
   }
 }
 
+export async function s3ObjectExistsAtPath(s3Path: string, s3Client: S3Client) {
+  const { bucket, key } = getBucketAndKeyFromS3Path(s3Path);
+  return s3ObjectExists(bucket, key, s3Client);
+}
+
 export async function getHeadObjectFromS3(bucket: string, key: string, s3Client: S3Client) {
   return await s3Client.send(
     new HeadObjectCommand({ Bucket: bucket, Key: key })
   );
+}
+
+export async function getHeadObjectFromS3Path(s3Path: string, s3Client: S3Client) {
+  const { bucket, key } = getBucketAndKeyFromS3Path(s3Path);
+  return getHeadObjectFromS3(bucket, key, s3Client);
 }
 
 export async function getSHA1OfObject(bucket: string, key: string, s3Client: S3Client) {
@@ -89,6 +126,11 @@ export async function getSHA1OfObject(bucket: string, key: string, s3Client: S3C
     throw new Error("Buffer was undefined");
   }
   return createHash("sha1").update(buffer).digest("hex");
+}
+
+export async function getSAH1OfObjectAtPath(s3Path: string, s3Client: S3Client) {
+  const { bucket, key } = getBucketAndKeyFromS3Path(s3Path);
+  return getSHA1OfObject(bucket, key, s3Client);
 }
 
 export function getEnvConfig() {
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,10 +3,17 @@ import {
   putObjectToS3,
   getObjectFromS3,
   getListObjectsFromS3,
-  s3FileExists,
+  s3ObjectExists,
   getHeadObjectFromS3,
   getEnvConfig,
   getSHA1OfObject,
+  getBucketAndKeyFromS3Path,
+  putObjectToS3Path,
+  getObjectFromS3Path,
+  getListObjectsFromS3Path,
+  s3ObjectExistsAtPath,
+  getHeadObjectFromS3Path,
+  getSAH1OfObjectAtPath,
 } from "../src/index.js";
 import { S3Client, type _Object, type S3ClientConfig } from "@aws-sdk/client-s3";
 import { createS3Client } from "mock-aws-s3-v3";
@@ -29,11 +36,13 @@ describe("getS3Client(config)", () => {
   });
 });
 
-describe("putFileToS3(bucket, key, file, s3Client)", () => {
+describe("putObject(bucket, key, file, s3Client)", () => {
   afterAll(() => {
-    fs.rm("test/s3_mock/dev/put_test", { recursive: true }, (err) => {
-      if (err != null) console.error(err);
-    });
+    try {
+      fs.rmSync("test/s3_mock/dev/put_test", { recursive: true });
+    } catch {
+
+    }
   });
 
   const mockClient = createS3Client({
@@ -50,7 +59,35 @@ describe("putFileToS3(bucket, key, file, s3Client)", () => {
   });
 });
 
-describe("getFileFromS3(bucket, key, s3Client)", () => {
+describe("putObjectToS3Path(s3Path, file, s3Client)", () => {
+  afterAll(() => {
+    fs.rmSync(
+      "test/s3_mock/dev/put_path_test",
+      { recursive: true },
+    );
+  });
+
+  it("Should write object using path", async () => {
+    const mockClient = createS3Client({
+      localDirectory: "./test/s3_mock",
+      bucket: "dev",
+    });
+
+    const file = fs.readFileSync("test/test.xml");
+
+    await putObjectToS3Path(
+      "dev/put_path_test/test.xml",
+      file,
+      mockClient,
+    );
+
+    fs.readFileSync(
+      "test/s3_mock/dev/put_path_test/test.xml",
+    );
+  });
+});
+
+describe("getObjectFromS3(bucket, key, s3Client)", () => {
 
   it("Should get the file from S3 (test/mock) successfully", async () => {
     const mockClient = createS3Client({
@@ -58,11 +95,33 @@ describe("getFileFromS3(bucket, key, s3Client)", () => {
       bucket: "dev",
     });
 
-    const s3File = await getObjectFromS3("dev", "get_test/test.xml", mockClient);
-    expect(s3File).not.toBeUndefined();
+    const s3Object = await getObjectFromS3("dev", "get_test/test.xml", mockClient);
+    expect(s3Object).not.toBeUndefined();
 
-    expect(await s3File.Body?.transformToString()).toBe(
+    expect(await s3Object.Body?.transformToString()).toBe(
       fs.readFileSync("test/s3_mock/dev/get_test/test.xml").toString(),
+    );
+  });
+});
+
+describe("getObjectFromS3Path(s3Path, s3Client)", () => {
+  it("Should fetch object using path", async () => {
+    const mockClient = createS3Client({
+      localDirectory: "./test/s3_mock",
+      bucket: "dev",
+    });
+
+    const obj = await getObjectFromS3Path(
+      "dev/get_test/test.xml",
+      mockClient,
+    );
+
+    expect(
+      await obj.Body?.transformToString(),
+    ).toBe(
+      fs.readFileSync(
+        "test/s3_mock/dev/get_test/test.xml",
+      ).toString(),
     );
   });
 });
@@ -124,20 +183,48 @@ describe("getListObjectsFromS3(bucket, prefix, s3Client)", () => {
   });
 });
 
-describe("s3FileExists(bucket, key, s3Client)", () => {
+describe("getListObjectsFromS3Path(s3Path, s3Client)", () => {
+  it("Should list objects using path", async () => {
+    const mockClient = createS3Client({
+      localDirectory: "./test/s3_mock",
+      bucket: "dev",
+    });
+
+    const stream = getListObjectsFromS3Path(
+      "dev/get_list_object_test",
+      mockClient,
+      {
+        delimiter: "delimiter",
+        maxKeys: 2,
+      },
+    );
+
+    const objects: _Object[] = [];
+
+    stream.on("data", (obj: _Object) => {
+      objects.push(obj);
+    });
+
+    await finished(stream);
+
+    expect(objects.length).toBe(100);
+  });
+});
+
+describe("s3ObjectExists(bucket, key, s3Client)", () => {
   const mockClient = createS3Client({
     localDirectory: "./test/s3_mock",
     bucket: "dev",
   });
 
   it("Should return true for an existing file", async () => {
-    expect(await s3FileExists("dev", "get_test/test.xml", mockClient)).toBe(
+    expect(await s3ObjectExists("dev", "get_test/test.xml", mockClient)).toBe(
       true,
     );
   });
 
   it("Should return false for a non existing file", async () => {
-    expect(await s3FileExists("dev", "get_test/bana.na", mockClient)).toBe(
+    expect(await s3ObjectExists("dev", "get_test/bana.na", mockClient)).toBe(
       false,
     );
 
@@ -147,7 +234,32 @@ describe("s3FileExists(bucket, key, s3Client)", () => {
     });
 
     expect(
-      await s3FileExists("banana", "banana/bana.na", mockClient2),
+      await s3ObjectExists("banana", "banana/bana.na", mockClient2),
+    ).toBe(false);
+  });
+});
+
+describe("s3ObjectExistsAtPath(s3Path, s3Client)", () => {
+  const mockClient = createS3Client({
+    localDirectory: "./test/s3_mock",
+    bucket: "dev",
+  });
+
+  it("Should return true for existing object", async () => {
+    expect(
+      await s3ObjectExistsAtPath(
+        "dev/get_test/test.xml",
+        mockClient,
+      ),
+    ).toBe(true);
+  });
+
+  it("Should return false for missing object", async () => {
+    expect(
+      await s3ObjectExistsAtPath(
+        "dev/get_test/missing.xml",
+        mockClient,
+      ),
     ).toBe(false);
   });
 });
@@ -172,6 +284,36 @@ describe("getHeadObjectFromS3(bucket, key, s3Client)", () => {
   it("Should throw an error trying to fetch the head of a non-existing object", async () => {
     await expect(
       getHeadObjectFromS3("dev", "put_test/test.xml", mockClient),
+    ).rejects.toThrow();
+  });
+});
+
+describe("getHeadObjectFromS3Path(s3Path, s3Client)", () => {
+  it("Should return head object", async () => {
+    const mockClient = createS3Client({
+      localDirectory: "./test/s3_mock",
+      bucket: "dev",
+    });
+
+    const res = await getHeadObjectFromS3Path(
+      "dev/get_test/test.xml",
+      mockClient,
+    );
+
+    expect(res.ContentLength).toBe(4542);
+  });
+
+  it("Should throw for missing object", async () => {
+    const mockClient = createS3Client({
+      localDirectory: "./test/s3_mock",
+      bucket: "dev",
+    });
+
+    await expect(
+      getHeadObjectFromS3Path(
+        "dev/missing.xml",
+        mockClient,
+      ),
     ).rejects.toThrow();
   });
 });
@@ -235,5 +377,68 @@ describe("getSHA1OfObject(bucket, key, s3Client)", () => {
     await expect(
       getSHA1OfObject("dev", "get_test/does_not_exist.xml", mockClient),
     ).rejects.toThrow();
+  });
+});
+
+describe("getSAH1OfObjectAtPath(s3Path, s3Client)", () => {
+  it("Should return SHA1 using path", async () => {
+    const mockClient = createS3Client({
+      localDirectory: "./test/s3_mock",
+      bucket: "dev",
+    });
+
+    const expected = createHash("sha1")
+      .update(
+        fs.readFileSync(
+          "test/s3_mock/dev/get_test/test.xml",
+        ),
+      )
+      .digest("hex");
+
+    expect(
+      await getSAH1OfObjectAtPath(
+        "dev/get_test/test.xml",
+        mockClient,
+      ),
+    ).toBe(expected);
+  });
+
+  it("Should throw for missing object", async () => {
+    const mockClient = createS3Client({
+      localDirectory: "./test/s3_mock",
+      bucket: "dev",
+    });
+
+    await expect(
+      getSAH1OfObjectAtPath(
+        "dev/get_test/does_not_exist.xml",
+        mockClient,
+      ),
+    ).rejects.toThrow();
+  });
+});
+
+describe("getBucketAndKeyFromS3Path(s3Path)", () => {
+  it("Should split bucket and key correctly", () => {
+    expect(
+      getBucketAndKeyFromS3Path("dev/folder/test.xml"),
+    ).toEqual({
+      bucket: "dev",
+      key: "folder/test.xml",
+    });
+  });
+
+  it("Should throw if bucket cannot be determined", () => {
+    expect(() =>
+      getBucketAndKeyFromS3Path(""),
+    ).toThrow();
+  });
+
+  it("Should throw if key is missing", () => {
+    expect(() =>
+      getBucketAndKeyFromS3Path("dev"),
+    ).toThrow(
+      "Could not determine key from the s3Path: dev",
+    );
   });
 });
